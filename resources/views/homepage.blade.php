c6db5cd057fd5fab17e4d867976cbab87a747062
Ian Silber <iansilber@gmail.com>
2015-09-27 15:15:50 -0700

    some front-end

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,16 +1,23 @@
-<?= View::make('partials/header'); ?>
+<?= View::make('partials/header', array('hideHome' => true)); ?>
 
-<div id="pushButton">
-	<a href="http://www.facebook.com/iansilber">
-		{!! HTML::image('images/sugarcereal_logo3.png', 'logo', array('width' => '200px', 'height' => '200px')); !!}
-		</a>
-</div>
+	<div class="row">
+		<div id="pushButton">
+				<a href="http://www.facebook.com/iansilber">{!! HTML::image('images/sugarcereal_logo3.png', 'logo', array('width' => '200px', 'height' => '200px')); !!}</a>
+		</div>
+	</div>
 
-<h3>Today's top 5 bids:</h3>
-<ul>
-<?php foreach ($bids as $bid) { ?>
-	<li><?= $bid->url ?>, <?= $bid->amount ?></li>
-<?php } ?>
-</ul>
+
+	<div class="row">
+		<div class="large-12 columns">
+
+			<ul class="inline-list">
+				<li><strong>Today's top 5 bids:</strong></li>
+			<?php foreach ($bids as $bid) { ?>
+				<li><?= $bid->url ?> $<?= $bid->amount ?></li>
+				<li> / </li>
+			<?php } ?>
+			</ul>
+		</div>
+	</div>
 
 <?= View::make('partials/footer'); ?>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -27,13 +27,14 @@
 <body>
 
   <nav class="top-bar" data-topbar role="navigation">
+
+    <? if (!isset($hideHome)) { ?>
     <ul class="title-area">
     <li class="name">
       <h1><a href="./">Sugar Cereal</a></h1>
     </li>
-     <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-  </ul>
+    </ul>
+    <? } ?>
 
   <section class="top-bar-section">
     <ul class="right">
