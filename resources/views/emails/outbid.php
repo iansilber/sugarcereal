5ca7271a87c9cd413b0f0f9f97a86fdda9afff99
Ian Silber <iansilber@gmail.comj>
2015-10-02 22:45:24 -0500

    bear's content changes

--- resources/views/emails/outbid.php
+++ resources/views/emails/outbid.php
@@ -1,3 +1,7 @@
-You've been outbid. 
+<p><a href=”https://youtu.be/8iOpUdsMeqM?t=33s”>Oh no!</a> You’ve been outbid on <a href=”http://www.sugarcereal.com”>Sugar Cereal.</a></p>
+<p>We don’t want you missing out on your day of glory. Head over to the <a href=”http://www.sugarcereal.com/bid”>bid page</a> to get back on top.</p>
+<p>Gooooooo get ‘em tiger,</p>
+<p>The Sugar Cereal Team</p>
 
-Place a new bid here: <?= action("HomeController@bid", array("url" => $bid->url)) ?>
+<!-- Place a new bid here: 
+<?= action("HomeController@bid", array("url" => $bid->url)) ?>   -->
